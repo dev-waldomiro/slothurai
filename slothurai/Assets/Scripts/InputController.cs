f361ec1c9dfdea62d145e033db01824c55c67b2d
Waldomiro Barbosa Romão <35855708+dev-waldomiro@users.noreply.github.com>
2020-07-10 23:12:09 -0300

    [ADD] PROJECT + NEW SCENE/SCRIPT/TEMPS
    Novo projeto com uma nova cena, um novo Script e novas imagens temporárias.

---
+++ slothurai/Assets/Scripts/InputController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)) 
+        {
+            //CALL I FUNCTION
+        }
+
+        if(Input.GetKeyDown(KeyCode.J)) 
+        {
+            //CALL J FUNCTION
+        }
+        
+        if(Input.GetKeyDown(KeyCode.K)) 
+        {
+            //CALL K FUNCTION
+        }
+
+        if(Input.GetKeyDown(KeyCode.L)) 
+        {
+            //CALL L FUNCTION
+        }
+    }
+
+}
