89d2b1052f4e231b3f96f3cde43e7cd1b4462345
Kajuero <klegerh@yahoo.com.br>
2020-07-11 19:57:56 -0300

    Pasue Menu Script

---
+++ slothurai/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameP = false;
+
+    public GameObject Pauseme;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameP)
+            {
+                RES();
+            }else
+            {
+                PAL();
+            }
+        }
+    }
+    void RES()
+    {
+        Pauseme.SetActive(false);
+        Time.timeScale = 1f;
+        GameP = false;
+    }
+    void PAL()
+    {
+        Pauseme.SetActive(true);
+        Time.timeScale = 0f;
+        GameP = true;
+    }
+}
