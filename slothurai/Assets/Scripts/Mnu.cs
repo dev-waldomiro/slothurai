727b06ef3fc79508b091046c5d82015f9e26dcef
Andrew Simmons <andrewsimmons007@hotmail.com>
2020-07-12 14:36:01 -0300

    Iha!

--- slothurai/Assets/Scripts/Mnu.cs
+++ slothurai/Assets/Scripts/Mnu.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 
 public class Mnu : MonoBehaviour
 {
+public AudioSource audioS;
+public AudioSource bgm;
+public AudioClip yooo;
+public Parallax pr;
+
     public void Game()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(Go());
+    }
+
+     IEnumerator Go(){
+        audioS.PlayOneShot(yooo);
+        bgm.Stop();
+        EventSystem.current.SetSelectedGameObject(null);
+        pr.Stop();
+        yield return new WaitForSeconds(yooo.length+.5f);
+
+        SceneManager.LoadScene("InterMission");
+        yield break;
     }
 }
