8fa99c55e53517704a8d5970566054e2b00423d5
Kajuero <klegerh@yahoo.com.br>
2020-07-12 04:57:04 -0300

    Sound Sistem

---
+++ slothurai/Assets/Scripts/audiomaneger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class audiomaneger : MonoBehaviour
+{
+    public Audio[] sounds;
+    void Awake()
+    {
+        foreach (Audio s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Test");
+    }
+
+    public void Play (string name)
+    {
+       Audio s = Array.Find(sounds, sound => sound.name == name);
+       s.source.Play();
+    }
+}
